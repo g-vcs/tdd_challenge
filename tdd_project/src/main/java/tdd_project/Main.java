9fe1e90344e9fcd1cb5727396bbc68aecbac3770
unknown <guilhermevcsousa@hotmail.com>
2023-01-14 15:02:06 +0000

    code done

--- tdd_project/src/main/java/tdd_project/Main.java
+++ tdd_project/src/main/java/tdd_project/Main.java
@@ -1,4 +1,11 @@
 package tdd_project;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Service service = new Service();
+        service.getNthUpperCharacter("ITCLiNicAl", -1);
+
+    }
 }
--- tdd_project/src/main/java/tdd_project/Service.java
+++ tdd_project/src/main/java/tdd_project/Service.java
@@ -1,4 +1,23 @@
 package tdd_project;
 
+import java.util.ArrayList;
+
 public class Service {
+
+    public String getNthUpperCharacter(String string, int n) {
+        StringBuilder result = new StringBuilder();
+
+        if (n < 1) {
+            return result.toString();
+        }
+
+        for (int i = n - 1; i < string.length(); i += n) {
+            if (Character.isUpperCase(string.charAt(i))) {
+                result.append(string.charAt(i));
+            }
+        }
+
+        System.out.println("result is: " + result);
+        return result.toString();
+    }
 }
